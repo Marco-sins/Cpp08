c74790126f57bd71406aff2748324085a0b525e0
Marco-sins <marcomembrillamelgar@gmail.com>
2026-03-03 10:36:19 +0100

    Ex02 done

---
+++ ex02/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "MutantStack.hpp"
+
+int main()
+{
+    MutantStack<int> mstack;
+    mstack.push(5); // 5
+    mstack.push(17); // 5 17
+    std::cout << mstack.top() << std::endl; // 17
+    mstack.pop(); // 5
+    std::cout << mstack.size() << std::endl; // 1
+    mstack.push(3); // 5 3
+    mstack.push(5); // 5 3 5
+    mstack.push(737); // 5 3 5 737 
+    mstack.push(0); // 5 3 5 737 0
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl; 
+        ++it;
+    }
+    std::stack<int> s(mstack);
+    return 0;
+}
